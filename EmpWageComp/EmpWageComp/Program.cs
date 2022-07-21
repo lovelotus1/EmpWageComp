b83f9b118fd829e468397ed62ca17df3809e24c8
lovelotus <opera.prem@gmail.com>
2022-07-21 14:26:53 +0530

    Added To save total wages by company-UC9

--- EmpWageComp/EmpWageComp/Program.cs
+++ EmpWageComp/EmpWageComp/Program.cs
@@ -1,10 +1,16 @@
 ﻿using EmpWageComputation;
 
 Console.WriteLine("Welcome to Employee Wage Computation Program\n");
-Console.WriteLine("Dell");
-EmpAttendance.GetMonthlyEmpWage("Dell", 20, 10, 5);
-Console.WriteLine("HP");
-EmpAttendance.GetMonthlyEmpWage("HP", 10, 20, 4);
-Console.WriteLine("Lenovo");
-EmpAttendance.GetMonthlyEmpWage("Lenovo", 15, 15, 3);
+Console.WriteLine("Dell".ToString());
+EmpAttendance Dell = new EmpAttendance("Dell", 10, 4, 20);
+Dell.GetMonthlyEmpWage();
+
+Console.WriteLine("HP".ToString());
+EmpAttendance HP = new EmpAttendance("HP", 20, 5, 20);
+HP.GetMonthlyEmpWage();
+
+Console.WriteLine("Lenovo".ToString());
+EmpAttendance Lenovo = new EmpAttendance("Lenovo", 30, 3, 30);
+Lenovo.GetMonthlyEmpWage();
+
 Console.ReadLine();
