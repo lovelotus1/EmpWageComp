b05a6b716afda2ade64a608267eb4e3c885bd775
lovelotus <opera.prem@gmail.com>
2022-07-21 14:15:38 +0530

    Added Employee Wage Multi Comp UC8

--- EmpWageComp/EmpWageComp/Program.cs
+++ EmpWageComp/EmpWageComp/Program.cs
@@ -1,5 +1,10 @@
 ﻿using EmpWageComputation;
 
-Console.WriteLine("Welcome to Employee Wage Computation Program");
-EmpAttendance.GetMonthlyEmpWage();
+Console.WriteLine("Welcome to Employee Wage Computation Program\n");
+Console.WriteLine("Dell");
+EmpAttendance.GetMonthlyEmpWage("Dell", 20, 10, 5);
+Console.WriteLine("HP");
+EmpAttendance.GetMonthlyEmpWage("HP", 10, 20, 4);
+Console.WriteLine("Lenovo");
+EmpAttendance.GetMonthlyEmpWage("Lenovo", 15, 15, 3);
 Console.ReadLine();
